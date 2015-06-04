4ccfe7b7a42338fe6ca6d80233720c901b2bb3ca
ahuber93 <andrewhuber93@icloud.com>
2015-06-04 17:16:10 -0400

    Read File Complete
    CSV file is now read correctly

--- src/line_of_best_fit.py
+++ src/line_of_best_fit.py
@@ -1,15 +1,30 @@
 import csv
 import sys
+import textwrap
 
 def readFile(fileName):
-	values=[][]
-	with open (fileName, 'rb') as csvFile:
-		reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-		for row in reader:
-			rowHalves=row.split(sep=',', maxsplit=1)
-			rowValues=[]
-			for half in rowHalves:
-				rowValues.append(float(half))
-			values.append(rowValues)
-	return values
+	times = []
+	thrust = []
+	f = open(fileName)
 
+	csv_f = csv.reader(f)
+	r = 0
+
+	for row in csv_f:
+		if len(row) >= 2:
+			times.insert(r, float(row[0]))
+			thrust.insert(r, float(row[1]))
+			r = r + 1
+
+	f.close
+	return (times, thrust)
+
+if len(sys.argv) < 3:
+	print ''
+	print textwrap.fill('The file path of the csv file containing the digitized ' + \
+		'thrust curve (the first argument) and the location of the output ' + \
+		'file (the second argument) must be provided.', 75)
+	print ''
+	print 'Try again...'
+else:
+	print readFile(sys.argv[1])
