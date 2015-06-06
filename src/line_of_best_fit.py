dfc8d14be7d4948e9a6c0684a5ae49853e48b84c
ahuber93 <andrewhuber93@icloud.com>
2015-06-06 17:16:44 -0400

    Commenting
    Commented code

--- src/line_of_best_fit.py
+++ src/line_of_best_fit.py
@@ -8,23 +8,25 @@ import textwrap
 from numpy import array
 from pylab import *
 
+# Reads the text file at a certain path (fileName)
 def readFile(fileName):
 	times = []
 	thrust = []
 	f = open(fileName)
 
 	csv_f = csv.reader(f)
-	r = 0
 
 	for row in csv_f:
-		if len(row) >= 2:
-			times.insert(r, float(row[0]))
-			thrust.insert(r, float(row[1]))
-			r = r + 1
+		if len(row) >= 2: # if there are at least two values (time and thrust)
+			times.append(float(row[0])) # append the time value in the times list
+			thrust.apppend(float(row[1])) # append the thrust value in the thrust list
 
 	f.close
 	return (numpy.array(times), numpy.array(thrust))
 
+# A traditional f(x) mathematical function. x is the x value in f(x) and the 
+# coefficients are the equation's coefficients (e.g., in y = ax + b, a and b are the 
+# coefficients)
 def f(x, coefficients):
 	degree = coefficients.size - 1.0
 	s = 0.0
@@ -35,6 +37,7 @@ def f(x, coefficients):
 
 	return s
 
+# Calculates the average of a list of values (values)
 def average(values):
 	s = 0.0
 	n = 0.0
@@ -45,6 +48,10 @@ def average(values):
 
 	return s / n
 
+# Calculates the maximum error, average error, and RMS error
+#	times - the time values in the thrust curve (in seconds)
+#	thrusts - the thrust values in the thrust curve (in newtons)
+#	coefficients - the coefficients in the line of best fit equation
 def calculateError(times, thrusts, coefficients):
 	errors = []
 	squareErrors = []
@@ -70,9 +77,12 @@ def calculateError(times, thrusts, coefficients):
 		rmsErr = abs(rmsErr)
 
 	return maxErr, avgErr, rmsErr
+# ======================================================================================================
+# SCRIPT STARTS HERE
+# ======================================================================================================
+warnings.simplefilter('ignore', numpy.RankWarning) # numpy raises a warning; we will ignore it
 
-warnings.simplefilter('ignore', numpy.RankWarning)
-
+# print an error message if not enough command line arguments were provided
 if len(sys.argv) < 4:
 	print ''
 	print 'The following three command line arguments must be provided:'
@@ -84,38 +94,51 @@ if len(sys.argv) < 4:
 	print ''
 	print 'Try again...'
 else:
+	# get the command line arguments
 	inputFileName = os.path.abspath(sys.argv[1])
 	outputFileName = os.path.abspath(sys.argv[2])
 	outputDir = os.path.abspath(sys.argv[3])
 
+	# make the output directory (the one that will contain the images of graphs) if it does not exist
 	if not os.path.exists(outputDir):
 		os.makedirs(outputDir)
+
+	# complete the file path of the output directory by appending a forward slash if already not completed
 	if not outputDir.endswith('/'):
 		outputDir = outputDir + '/'
 
-	times, thrust = readFile(inputFileName)
-	startDegree = 3
-	endDegree = 17
-	outputFile = open(outputFileName, 'w')
+	times, thrust = readFile(inputFileName) # read the csv file
+	startDegree = 3 # the lowest degree that we will use
+	endDegree = 17 # the highest degree that we will use
+	outputFile = open(outputFileName, 'w') # open the output text file; 
+										   # create it if necessary; 
+										   # overwrite if necessary
 
 	for degree in range(startDegree, endDegree + 1):
-		p = np.array(numpy.polyfit(times, thrust, degree))
+		p = np.array(numpy.polyfit(times, thrust, degree)) # calculate line of best fit
+
+		# make plot
 		pylab.plot(times, thrust, 'bo')
 		pylab.xlabel('Times (in seconds)')
 		pylab.ylabel('Thrust (in newtons)')
 		pylab.grid(True)
 		pylab.title('Thrust Data: Degree = {0:2d}'.format(degree))
 		pylab.plot(times, polyval(p, times), 'g')
-		filePath = outputDir + 'polyfit_degree_' + str(degree);
-		pylab.savefig(filePath)
-		pylab.close()
 
-		maxErr, avgErr, rmsErr = calculateError(times, thrust, p)
-		outputFile.write('degree = {0:2d}: maxError = {1:.2f}\n'.format(degree, maxErr))
+		filePath = outputDir + 'polyfit_degree_' + str(degree); # make file path of image
+		pylab.savefig(filePath) # save image
+		pylab.close() # close current figure so a new one can be made
+
+		maxErr, avgErr, rmsErr = calculateError(times, thrust, p) # calculate the errors
+
+		# write results to output file
+		outputFile.write('degree = {0:2d}: maxError = {1:.2f}\n'.format(degree, maxErr)) 
 		outputFile.write('degree = {0:2d}: avgError = {1:.2f}\n'.format(degree, avgErr))
 		outputFile.write('degree = {0:2d}: rmsError = {1:.2f}\n\n'.format(degree, rmsErr))
 
-		progress = ((degree - startDegree + 0.0) / (endDegree - startDegree)) * 100
-		print '{0:.2f}% complete...'.format(progress)
+		# calculate current progress of program execution
+		progress = ((degree - startDegree + 0.0) / (endDegree - startDegree)) * 100 
+		print '{0:.2f}% complete...'.format(progress) # print progress
 
+	outputFile.close # close the output file
 	print 'Done!'
